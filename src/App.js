32b3387c2c68a002eed7a9353e4cc69f8c2c3858
kundankumaryadav88 <karanjashveersingh88@gmail.com>
2024-02-07 12:46:39 +0530

    cgejcw

--- src/App.js
+++ src/App.js
@@ -1,12 +1,8 @@
 import './App.css';
 import Hero from './herosection/Hero';
 import Navbar from './navbar/Navbar';
-// import Section from './cardsection/Section';
-// import Cardview from './cardsection/Cardview';
-// import CustomCard from './CustomCard';
 import GridComponents from './GridComponents';
 import NewAlbum from './NewAlbum';
-import SwiperComponents from './CarouselSec';
 
 
 function App() {
